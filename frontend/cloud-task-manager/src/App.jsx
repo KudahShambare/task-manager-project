ebac39ec5b9f04d27767ce946d6f0a2ff62b50c8
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2026-05-12 00:55:26 +0200

    added a login page

--- frontend/cloud-task-manager/src/App.jsx
+++ frontend/cloud-task-manager/src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import { Login } from "./pages/Auth";
 
-import './App.css'
+// Pages
 
 function App() {
-
   return (
-    <>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
 
-      
-     
+        <Route path="/login" element={<Login/>} />
 
-      
-    </>
-  )
+        {/* 404 Route */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/cloud-task-manager/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+/***Home Page */
+
+/**
+ * Welcome 
+ */
+
+import { Login } from "./Auth";
+
+const Home = ()=>{
+    return <>
+    <h1>Cloud Task Manager</h1>
+    <Login/>
+    </>
+}
+export default Home;
